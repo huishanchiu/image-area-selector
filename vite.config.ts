4cd9a7b6dc8815dde8b56a013fcd1f05a208a696
joychiuu <joy.chiu@urmart.com>
2024-08-28 17:38:50 +0800

    chore:install packages and define the path of components

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@component": path.resolve(__dirname, "src/component"),
+      "@constant": path.resolve(__dirname, "src/constant"),
+      "@type": path.resolve(__dirname, "src/type"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@context": path.resolve(__dirname, "src/context"),
+    },
+  },
 });
